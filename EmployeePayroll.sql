7452d5acfc70be838f506055e0dcefaa01fb23e1
Prashantbhure50 <prashantbhure50@gmail.com>
2021-05-21 22:23:19 +0530

    [Prashant] Table Created

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -5,6 +5,8 @@ use Payroll_service13
 select DB_NAME();
 
 create table employeePayroll(
-
-
+id int Identity(1,1) primary key,
+name varchar(100) not null,
+salary money not null,
+start date not null
 )
